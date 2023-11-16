a909916192f930ea847dcb6d4e467913e0e346f7
Jhair Alexby <jhair.alexby1@gmail.com>
2023-11-16 12:21:39 -0600

    implementacion de componente Guide

---
+++ src/app/components/Guide.js
@@ -0,0 +1,12 @@
+import Calendario from "./Calendario";
+
+const Guide = () => {
+    return (
+        <div>
+            <h1>guia</h1>
+        </div>
+    );
+};
+
+
+export default Guide;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Login from "@/app/components/Login";
 import React from "react";
 import Content from "@/app/components/Content";
+import Guide from "@/app/components/Guide";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <Content/>
+        <Guide/>
     </main>
   )
 }
