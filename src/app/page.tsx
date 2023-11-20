d149031f9bb0497d91b348629a78a1f3ba3504cc
Jhair Alexby <jhair.alexby1@gmail.com>
2023-11-19 19:47:18 -0600

    opcion de guia agregada y funcional en el HomePage.js bug encontrado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import HomePage from "@/app/components/HomePage";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <HomePage/>
+        <Content/>
     </main>
   )
 }
